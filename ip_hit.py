2eff16f645459456ce21047b3ba9c5ad03a0c643
jeebe <jeebemathew@gmail.com>
2020-08-04 17:45:34 +0000

    created a dictionary with all Ip address and it's hits

--- ip_hit.py
+++ ip_hit.py
@@ -1,6 +1,18 @@
 logfile = '/root/python/access.log'
+ipcounter = {}
+
 log = open(logfile)
+
 for logline in log:
-    print(logline.split()[0])
-    break
+
+    ip = logline.split()[0]
+    if ip not in ipcounter:
+     
+        ipcounter[ip] = 1
+       
+    else:
+
+        ipcounter[ip] = ipcounter[ip] + 1
+        
+print(ipcounter)
 log.close()
